f772a0a79ab978b08106f12dc710c52d51c08a67
System Administrator <admin@example.com>
2025-09-24 12:55:32 -0700

    Added generated sys_scope_privilege

---
+++ src/fluent/generated/sys_scope_privilege_6089527f2b883210c77ef2324291bf70.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+CrossScopePrivilege({
+    $id: Now.ID['6089527f2b883210c77ef2324291bf70'],
+    operation: 'execute',
+    status: 'allowed',
+    targetName: 'Glide API: user roles and groups',
+    targetScope: 'global',
+    targetType: 'scriptable',
+})
